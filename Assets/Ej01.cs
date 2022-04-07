5c0b49867ca1c92a4b1914460a012959350f980d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-04-07 14:11:29 -0300

    Exercise 1 completed

--- Assets/Ej01.cs
+++ Assets/Ej01.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections;
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores 
+//    enteros y responda si son iguales o no lo son.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ej01 : MonoBehaviour
 {
+    public int valor1;
+    public int valor2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (valor1 == valor2)
+        {
+            Debug.Log("Son iguales");
+        }
+        else
+        {
+            Debug.Log("NO son iguales");
+        }
     }
 
     // Update is called once per frame
