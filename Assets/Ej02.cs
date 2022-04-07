bbd8599da9abe671557d59de863cc09e5e54ecf3
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-04-07 14:14:02 -0300

    Exercise  2 started

--- Assets/Ej02.cs
+++ Assets/Ej02.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections;
+﻿//2. Crear las variables necesarias para ingresar por Inspector 
+//    el precio de 3 productos y un monto de dinero 
+//    disponible. Comunicar si la suma de los mismos supera 
+//    o no el monto de dinero disponible.Luego indicar cuánto 
+//    dinero sobra o falta.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ej02 : MonoBehaviour
 {
+
+    public int precio1;
+    public int precio2;
+    public int precio3;
+
+    public int dineroDisponible;
+
     // Start is called before the first frame update
     void Start()
     {
